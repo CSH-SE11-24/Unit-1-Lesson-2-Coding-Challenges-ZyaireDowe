125593d7d592ce1da4c9b32fbee13fcf2c7a4f71
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-09 09:54:45 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Work in pairs to complete the tasks as specified by the comments. Remember to follow driver-navigator model for group work, which means only one of you needs to complete the Repl.it. 
+
+Roles:
+- Navigator: References notes/documentation to gives instructions. Usually is more comfortable with the material.
+- Driver: Only person with tech out and is coding instructions.
---
+++ index.js
@@ -0,0 +1,58 @@
+// VARIABLES
+// For the navigator (person giving instructions),
+// Define three variables: `name`, `numberOfSiblings`, and `isBilingual`. Determine which should be constants and which may change.
+let name = "justice"
+let siblings = 4
+let bilingual = true 
+
+
+
+
+
+
+// Print these variables to the console.
+
+console.log(name)
+console.log(siblings)
+console.log(bilingual)
+
+// RE-ASSIGNMENT
+// Create a variable `counter` and initialize it with 0.
+let counter = 0
+// Set it to a larger value
+counter = 1
+// Increment the `counter` by 1 without specifying the number!
+counter = counter + 1
+// Print the updated value of `counter`.
+console
+
+// DATA TYPES
+// Create variables for various primitive data types: `number`, `string`, `boolean`, and `null`.
+
+
+
+
+
+// Print the values and confirm their type using typeof
+
+
+
+
+// Create a variable that has an undefined type 
+
+// Print the variable and confirm its type
+
+
+
+// EXTRA CREDIT (Google these!)
+// Make an array of siblings/languages that you speak
+
+
+
+// Make an object with your name, siblings, and languages
+
+
+
+// Try printing the number of siblings you have using ONLY the object (Hint: look up how to find the length of an array)
+
+
